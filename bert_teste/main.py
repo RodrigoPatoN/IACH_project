3a35111c9c3a9e0e65800994945451c37e9cc47c
RaulSofia <rauljcsofia@gmail.com>
2023-12-03 06:14:27 +0000

    stable, ready to run random strategy

--- bert_teste/main.py
+++ bert_teste/main.py
@@ -3,15 +3,17 @@ This is the main file for the active learning project.
 Implements the main loop of the active learning algorithm.
 """
 #HYPERPARAMETERS
+STRATEGY = 'random' #active learning strategy to be used. options: random, ...
 INITIAL_POOL_SIZE = 1000 #initial pool size
 ANNOTATION_SIZE = 100 #number of entries to be annotated in each iteration
-MAX_POOL_SIZE = 10000 #maximum pool size. if not compatible with INITIAL_POOL_SIZE and ANNOTATION_SIZE, the last annotation will be shorter than ANNOTATION_SIZE
+MAX_POOL_SIZE = 1000 #maximum pool size. if not compatible with INITIAL_POOL_SIZE and ANNOTATION_SIZE, the last annotation will be shorter than ANNOTATION_SIZE
 
 EPOCHS = 1 #number of epochs to be used in each iteration
 
 
 from copy import deepcopy
 from math import ceil
+import os
 import numpy as np
 from oracle import Oracle
 import sklearn.model_selection as sk
@@ -35,6 +37,19 @@ def launch_subprocess_and_wait(command):
 
 
 def main():
+
+    #initialize run
+    print('initializing run')
+    run_nonce = 10000000000 - int(time.time()) #this is a nonce to be used in the run name. subtraction because this way the most recent is at the top of the list
+    run_name = f'run_{run_nonce}'
+    print(f'run name: {run_name}')
+    os.mkdir(f'./results/{run_name}')
+    os.mkdir(f'./models/{run_name}')
+    placeholder_results = pd.read_csv('./results/expected_results.csv', comment='#')
+    placeholder_results.to_csv(f'./results/{run_name}/results.csv', index=False)
+
+
+
     #initialize datasets
     train_dataset, test_dataset = sk.train_test_split(pd.read_csv("./datasets/Sentiment Analysis Dataset.csv", on_bad_lines="skip"), test_size=0.2)
     test_dataset = test_dataset.sample(1000)
@@ -42,22 +57,35 @@ def main():
     oracle = Oracle(train_dataset, ['SentimentText'], ['Sentiment'])
     print('dataset loaded to oracle')
 
-    #initialize training pool
-    print('initializing training pool')
-    x, y = oracle.random_pick(INITIAL_POOL_SIZE)
+    
+    
     
 
     #main active learning loop
 
     n_iterations = ceil((MAX_POOL_SIZE - INITIAL_POOL_SIZE) / ANNOTATION_SIZE) + 1
+    rest = (MAX_POOL_SIZE - INITIAL_POOL_SIZE) % ANNOTATION_SIZE
     for i in range(n_iterations):
         if i == n_iterations - 1:
-            annotation_size = MAX_POOL_SIZE - len(x)
+            annotation_size = rest
+        elif i == 0:
+            annotation_size = INITIAL_POOL_SIZE
+            #initialize training pool
+            print('initializing training pool')
         else:
             annotation_size = ANNOTATION_SIZE
 
         #annotate the pool
-        attributes, labels = oracle.random_pick(annotation_size)
+        if STRATEGY == 'random':
+            attributes, labels = oracle.random_pick(annotation_size)
+        
+        #insert new strategies here
+
+        else:
+            print(f'{STRATEGY} strategy is not implemented')
+            return
+        
+        
         print(f'Iteration {i}/{n_iterations}\nSize of training pool: {oracle.get_annotated_size()}')
 
         
@@ -67,6 +95,19 @@ def main():
         print('retraining the model')
         nonce = int(time.time())
         launch_subprocess_and_wait(" ".join(['python', 'train_bert.py', './datasets/temp_train_dataset.csv', './datasets/temp_test_dataset.csv', f'./models/bert_{nonce}.h5', str(EPOCHS), str(32)]))
+
+        #read the results from the temp file
+        last_results = pd.read_csv('./results/temp_results.csv', comment='#')
+        last_results['run_name'] = [run_name for _ in range(len(last_results))] #actually should just be a one value list
+        last_results['active_learning_strategy'] = [STRATEGY for _ in range(len(last_results))]
+        dataset_size = oracle.get_annotated_size()
+        last_results['dataset_size'] = [dataset_size for _ in range(len(last_results))]
+        print("appending results to results.csv")
+        results = pd.read_csv(f'./results/{run_name}/results.csv', comment='#')
+        results = pd.concat([results, last_results], axis=0, ignore_index=True)
+        results.to_csv(f'./results/{run_name}/results.csv', index=False)
+
+        print("results appended to results.csv")
 
 
 
--- bert_teste/train_bert.py
+++ bert_teste/train_bert.py
@@ -1,8 +1,20 @@
-"""Helper script that trains a bert model, based on the "train_dataset file" and evaluates it
-Saves it using a nonce name
+"""
+Helper script that trains a bert model, based on the command line parameters it acepts.
+It is supposed to be launched as a child process from the main script, guarantees isolation
+between runs, which reduces problems with memory leaks caused by tensorflow between runs (cache i believe)
+Saves the model using a nonce name provided
+Saves metrics to a temporary file. It will be overwritten on each run, so process it between runs.
+
+Why does it write to a temp file instead of directly to a designated results file? Seems easier to manage all that logic
+inside the parent process, and it also allows for more flexibility, since the parent process can decide what to do with
+the results and where to put them.
+This way, each main run can have its own output file, that will have the results of all the bert runs it launched. Therefore,
+as we will have lots of test runs, we will have several output files, and only merge the ones that are actual runs to the main results file.
 """
 
+
 import argparse
+import os
 import sys
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
@@ -12,29 +24,56 @@ def main(train_dataset_path, test_dataset_path, output_path, epochs, batch_size)
     import pandas as pd
     from model import BERTModel
 
-    output_filename = output_path.split('/')[-1].split('.')[0] + ".out"
+    output_filename = "temp_results.csv"
     print(f"Saving output to {output_filename}")
 
-    with open("/home/raulsofia/IACH/bert_teste/results/" + output_filename, "w") as f:
-        sys.stdout = f
-        bert = BERTModel.from_file('./models/initial_bert.h5')
-        bert.summary()
-        train_dataset = pd.read_csv(train_dataset_path, on_bad_lines="skip")
-        test_dataset = pd.read_csv(test_dataset_path, on_bad_lines="skip")
-        bert.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        bert.fit(train_dataset['SentimentText'], train_dataset['Sentiment'], epochs=epochs, batch_size=batch_size)
 
+    #training
+    bert = BERTModel.from_file('./models/initial_bert.h5')
+    bert.summary()
+    train_dataset = pd.read_csv(train_dataset_path, on_bad_lines="skip")
+    test_dataset = pd.read_csv(test_dataset_path, on_bad_lines="skip")
+    bert.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    bert.fit(train_dataset['SentimentText'], train_dataset['Sentiment'], epochs=epochs, batch_size=batch_size)
+
+    #testing
+    with open(os.path.join("./results/", output_filename), 'w') as f:
+        f.write("#This is a temporary file. Will be overwritten on each training run.\n"
+                "#It contains all the metrics for the last run, so process it before running again.\n"
+                "#Read it with pandas.read_csv('temp_results.out', comment='#')\n")
         preds = bert.predict(test_dataset['SentimentText']).flatten().round()
-        print(preds.shape)
-        print(test_dataset['Sentiment'].to_numpy().shape)
-        accuracy = accuracy_score(test_dataset['Sentiment'].to_numpy(), preds)
-        f1 = f1_score(test_dataset['Sentiment'].to_numpy(), preds)
-        precision = precision_score(test_dataset['Sentiment'].to_numpy(), preds)
-        recall = recall_score(test_dataset['Sentiment'].to_numpy(), preds)
-        print(f'Accuracy: {accuracy}\nF1: {f1}\nPrecision: {precision}\nRecall: {recall}')
+        # print(preds.shape)
+        # print(test_dataset['Sentiment'].to_numpy().shape)
+        y_target = test_dataset['Sentiment'].to_numpy()
+        
+        #metrics
+        accuracy = accuracy_score(y_target, preds)
+        precision_avg = precision_score(y_target, preds, average='weighted')
+        recall_avg = accuracy #could recalculate but there's no need, it is the same as accuracy. just here to remember myself of that
+        f1_avg = f1_score(y_target, preds, average='weighted')
+
+        precision_negative, precision_positive = precision_score(y_target, preds, average=None)
+        recall_negative, recall_positive = recall_score(y_target, preds, average=None)
+        f1_negative, f1_positive = f1_score(y_target, preds, average=None)
+        
+        results = pd.DataFrame({
+            'accuracy': [accuracy],
+            'precision_avg': [precision_avg],
+            'recall_avg': [recall_avg],
+            'f1_avg': [f1_avg],
+            'precision_negative': [precision_negative],
+            'precision_positive': [precision_positive],
+            'recall_negative': [recall_negative],
+            'recall_positive': [recall_positive],
+            'f1_negative': [f1_negative],
+            'f1_positive': [f1_positive]
+        })
+        f.write(results.to_csv(index=False))
+        
         
         # bert.evaluate(test_dataset['SentimentText'], test_dataset['Sentiment'])
-        bert.save(output_path)
+        #not saving models due to space constraints
+        # bert.save(output_path)
 
 
 def main_test():
@@ -42,12 +81,14 @@ def main_test():
 
 
 if __name__ == "__main__":
-    # main_test()
-    parser = argparse.ArgumentParser(description='Train a bert model and save it with a nonce name')
-    parser.add_argument('train_dataset_path', type=str, help='path to the train dataset')
-    parser.add_argument('test_dataset_path', type=str, help='path to the test dataset')
-    parser.add_argument('output_path', type=str, help='path to the output file')
-    parser.add_argument('epochs', type=int, help='number of epochs to train', default=1)
-    parser.add_argument('batch_size', type=int, help='batch size', default=32)
-    args = parser.parse_args()
-    main(args.train_dataset_path, args.test_dataset_path, args.output_path, args.epochs, args.batch_size)
+    if len(sys.argv) > 1: #running from main.py, with args
+        parser = argparse.ArgumentParser(description='Train a bert model and save it with a nonce name')
+        parser.add_argument('train_dataset_path', type=str, help='path to the train dataset')
+        parser.add_argument('test_dataset_path', type=str, help='path to the test dataset')
+        parser.add_argument('output_path', type=str, help='path to the output file')
+        parser.add_argument('epochs', type=int, help='number of epochs to train', default=1)
+        parser.add_argument('batch_size', type=int, help='batch size', default=32)
+        args = parser.parse_args()
+        main(args.train_dataset_path, args.test_dataset_path, args.output_path, args.epochs, args.batch_size)
+    else: #testing, running this script directly, no args
+        main_test()
